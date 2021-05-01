93b150b75e938dbdf7cb5708011a52d7ed5375dd
acneuromancer <dokukodo108@gmail.com>
2021-05-02 00:27:22 +0200

    Add infection handling and colouring people according their health state.

--- epidemic_simulator.py
+++ epidemic_simulator.py
@@ -1,14 +1,25 @@
+# Creating virtual environment:
+# python3 -m venv env
+# source /env/bin/activate
+
+
 import pygame
 import sys
 from random import randint
 
 # Constants
 SCREEN_SIZE = WIDTH, HEIGHT = (1280, 720)
-NUMBER_OF_PEOPLE = 10
+NUMBER_OF_PEOPLE = 100
 PERSON_SPEED = 15
 PERSON_COLOR_CLEAR = (200, 150, 0)
 PERSON_COLOR_INFECTED = (200, 0, 0)
 PERSON_COLOR_IMMUNE = (0, 150, 0)
+PERSON_AURA = 20
+ILLNESS_TIME = 500
+IMMUNE_TIME = 800
+INFECTION_PROBABILITY_WITH_MASK = 50
+INFECTION_PROBABILITY_WITHOUT_MASK = 2
+FIRST_CONTACT = 2
 
 # Initialization
 pygame.init()
@@ -26,33 +37,68 @@ class Person:
         self.y = y
         self.dx = randint(-1, 1)
         self.dy = randint(-1, 1)
+        self.illness = 0
+        self.immune = 0
+        self.mask = False
+        
 
     def move(self):
         if randint(1, 30) == 1:
             self.dx = randint(-1, 1)
         if randint(1, 30) == 1:
             self.dy = randint(-1, 1)
-
-        self.x += self.dx
-        self.y += self.dy
-
+        if PERSON_AURA < self.x + self.dx < WIDTH - PERSON_AURA:
+            self.x += self.dx
+        if PERSON_AURA < self.y + self.dy < HEIGHT - PERSON_AURA:
+            self.y += self.dy
+    
+    def cure(self):
+        if self.illness > 0:
+            self.illness -= 1
+            if self.illness == 0:
+                self.immune = IMMUNE_TIME
+        if self.immune > 0:
+            self.immune -= 1
+            
 
 def draw(people):
     screen.fill((0, 0, 100))
+    
     for person in people:
-        pygame.draw.circle(screen, PERSON_COLOR_CLEAR, (person.x, person.y), 20, 0)
+        if person.illness > 0:
+            _color = PERSON_COLOR_INFECTED
+        elif person.immune > 0:
+            _color = PERSON_COLOR_IMMUNE
+        else:
+            _color = PERSON_COLOR_CLEAR
+        pygame.draw.circle(screen, _color, (person.x, person.y), PERSON_AURA, 0)
+    
     pygame.display.update()
     fps.tick(PERSON_SPEED)
 
 def modify(people):
     for person in people:
         person.move()
-
+        person.cure()
+        for other_person in people:
+            if id(person) != id(other_person):
+                if other_person.x - 2 * PERSON_AURA < person.x < other_person.x + 2 * PERSON_AURA \
+                        and other_person.y - 2 * PERSON_AURA < person.y < other_person.y + 2 * PERSON_AURA :
+                    if other_person.illness > 0 and person.illness == 0 and person.immune == 0:
+                        if person.mask or other_person.mask:
+                            if randint(1, INFECTION_PROBABILITY_WITH_MASK) == 1:
+                                person.illness = ILLNESS_TIME
+                        else:
+                            if randint(1, INFECTION_PROBABILITY_WITHOUT_MASK) == 1:
+                                person.illness = ILLNESS_TIME
+        
 # Create people
 main_people_list = [Person(randint(50, WIDTH - 50), randint(50, HEIGHT - 50))
                     for i in range(NUMBER_OF_PEOPLE)]
 
-
+# The first infection
+for i in range(FIRST_CONTACT):
+    main_people_list[i].illness = ILLNESS_TIME 
 
 # Main program
 while True:
